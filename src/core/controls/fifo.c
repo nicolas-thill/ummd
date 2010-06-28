ae94b6e0cdebfd8f9270d47828f532f522286315
Nico <nicolas.thill@gmail.com>
2010-06-28 03:33:15 +0200

    core/control/fifo: add event handling, don't break close func on error

--- src/core/controls/fifo.c
+++ src/core/controls/fifo.c
@@ -26,6 +26,9 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
+
+#include <event.h>
 
 #include "core/controls.h"
 
@@ -38,9 +41,31 @@ struct my_control_priv {
 	my_control_t base;
 	char *path;
 	int fd;
+	struct event event;
 };
 
 #define MY_CONTROL_PRIV(p) ((my_control_priv_t *)(p))
+
+#define MY_CONTROL_BUF_SIZE  1024
+
+static void my_control_fifo_event_handler(int fd, short event, void *p)
+{
+	my_control_t *control = (my_control_t *)p;
+	char buf[MY_CONTROL_BUF_SIZE + 1];
+	int n;
+
+	n = read(fd, buf, MY_CONTROL_BUF_SIZE);
+	if (n == -1) {
+		my_log(MY_LOG_ERROR, "core/control/fifo: error reading from fifo '%s' (%s)", MY_CONTROL_PRIV(control)->path, strerror(errno));
+	}
+	buf[MY_CONTROL_BUF_SIZE] = '\0';
+	if (n > 0) {
+		if (buf[n - 1] == '\n') {
+			buf[n - 1] = '\0';
+		}
+		my_log(MY_LOG_ERROR, "core/control/fifo: received '%s' from fifo '%s'", buf, MY_CONTROL_PRIV(control)->path);
+	}
+}
 
 static my_control_t *my_control_fifo_create(my_control_conf_t *conf)
 {
@@ -56,7 +81,7 @@ static my_control_t *my_control_fifo_create(my_control_conf_t *conf)
 	p = strchr(conf->url, ':');
 	if (p) {
 		n = p - conf->url;
-		if (strncmp(conf->url, "file", n) != 0) {
+		if ((strncmp(conf->url, "file", n) != 0) && (strncmp(conf->url, "fifo", n) != 0)) {
 			my_log(MY_LOG_ERROR, "core/control/fifo: unknown method '%.2$*1$s' in url '%$1s'", conf->url, n);
 			goto _MY_ERR_parse_url;
 		}
@@ -95,6 +120,9 @@ static int my_control_fifo_open(my_control_t *control)
 		goto _MY_ERR_open_fifo;
 	}
 
+	event_set(&(MY_CONTROL_PRIV(control)->event), MY_CONTROL_PRIV(control)->fd, EV_READ | EV_PERSIST, my_control_fifo_event_handler, control);
+	my_core_event_add(control->core, &(MY_CONTROL_PRIV(control)->event));
+	
 	return 0;
 
 _MY_ERR_open_fifo:
@@ -104,16 +132,16 @@ _MY_ERR_create_fifo:
 
 static int my_control_fifo_close(my_control_t *control)
 {
+	my_core_event_del(control->core, &(MY_CONTROL_PRIV(control)->event));
+
 	MY_DEBUG("core/control/fifo: closing fifo '%s'", MY_CONTROL_PRIV(control)->path);
 	if (close(MY_CONTROL_PRIV(control)->fd) == -1) {
 		my_log(MY_LOG_ERROR, "core/control/fifo: error closing fifo '%s' (%s)", MY_CONTROL_PRIV(control)->path, strerror(errno));
-		return -1;
 	}
 
 	MY_DEBUG("core/control/fifo: removing fifo '%s'", MY_CONTROL_PRIV(control)->path);
 	if (unlink(MY_CONTROL_PRIV(control)->path) == -1) {
 		my_log(MY_LOG_ERROR, "core/control/fifo: error removing fifo '%s' (%s)", MY_CONTROL_PRIV(control)->path, strerror(errno));
-		return -1;
 	}
 
 	return 0;
--- src/core/controls/sock.c
+++ src/core/controls/sock.c
@@ -23,9 +23,9 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <unistd.h>
 
 #include "core/controls.h"
 

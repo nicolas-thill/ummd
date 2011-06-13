d2945bbd0154335f6ace7dff6466ce432561b042
Nicolas Thill <nicolas.thill@gmail.com>
2011-06-13 17:03:31 +0200

    [io/all] cosmetic: reorder sources/targets, fix log message

--- src/core/io/all.c
+++ src/core/io/all.c
@@ -30,18 +30,119 @@
 #include "util/mem.h"
 #include "util/prop.h"
 
-static my_list_t my_targets;
 static my_list_t my_sources;
+static my_list_t my_targets;
+
+#define MY_SOURCE_REGISTER(x) { \
+	extern my_port_impl_t my_source_##x; \
+	my_port_impl_register(&my_sources, &my_source_##x); \
+}
 
 #define MY_TARGET_REGISTER(x) { \
 	extern my_port_impl_t my_target_##x; \
 	my_port_impl_register(&my_targets, &my_target_##x); \
 }
 
-#define MY_SOURCE_REGISTER(x) { \
-	extern my_port_impl_t my_source_##x; \
-	my_port_impl_register(&my_sources, &my_source_##x); \
+
+static my_port_impl_t *my_source_impl_find(my_port_conf_t *conf)
+{
+	my_port_impl_t *impl;
+	my_node_t *node;
+	char *impl_name;
+
+	impl_name = my_prop_lookup(conf->properties, "type");
+	if (!impl_name) {
+		impl_name = "file";
+	}
+
+	impl = my_port_impl_lookup(&my_sources, impl_name);
+	if (!impl) {
+		my_log(MY_LOG_ERROR, "core/%s: unknown type '%s'", conf->name, impl_name);
+	}
+
+	return impl;
 }
+
+static int my_source_create_fn(void *data, void *user, int flags)
+{
+	my_core_t *core = MY_CORE(user);
+	my_port_t *port;
+	my_port_conf_t *conf = MY_PORT_CONF(data);
+	my_port_impl_t *impl;
+
+	impl = my_source_impl_find(conf);
+	if (!impl) {
+		return 0;
+	}
+
+	port = my_port_create(core, conf, impl);
+	if (port) {
+		my_list_enqueue(core->sources, port);
+	}
+
+	return 0;
+}
+
+int my_source_create_all(my_core_t *core, my_conf_t *conf)
+{
+	MY_DEBUG("core/sources: creating all");
+	return my_list_iter(conf->sources, my_source_create_fn, core);
+}
+
+int my_source_destroy_all(my_core_t *core)
+{
+	MY_DEBUG("core/sources: destroying all");
+	return my_port_destroy_all(core->sources);
+}
+
+int my_source_open_all(my_core_t *core)
+{
+	MY_DEBUG("core/sources: opening all");
+	return my_port_open_all(core->sources);
+}
+
+int my_source_close_all(my_core_t *core)
+{
+	MY_DEBUG("core/sources: closing all");
+	return my_port_close_all(core->sources);
+}
+
+
+void my_source_register_all(void)
+{
+	MY_SOURCE_REGISTER(file);
+	MY_SOURCE_REGISTER(udp);
+/*
+	MY_SOURCE_REGISTER(sock);
+	MY_SOURCE_REGISTER(http_client);
+	MY_SOURCE_REGISTER(rtp_client);
+*/
+}
+
+#ifdef MY_DEBUGGING
+
+static int my_source_dump_fn(void *data, void *user, int flags)
+{
+	my_port_impl_t *impl = MY_PORT_IMPL(data);
+
+	MY_DEBUG("\t{");
+	MY_DEBUG("\t\tname=\"%s\";", impl->name);
+	MY_DEBUG("\t\tdescription=\"%s\";", impl->desc);
+	MY_DEBUG("\t}%s", flags & MY_LIST_ITER_FLAG_LAST ? "" : ",");
+
+	return 0;
+}
+
+void my_source_dump_all(void)
+{
+	MY_DEBUG("# registered sources");
+	MY_DEBUG("sources = (");
+	my_list_iter(&my_sources, my_source_dump_fn, NULL);
+	MY_DEBUG(");");
+
+}
+
+#endif /* MY_DEBUGGING */
 
 
 static my_port_impl_t *my_target_impl_find(my_port_conf_t *conf)
@@ -141,105 +242,6 @@ void my_target_dump_all(void)
 	MY_DEBUG("# registered targets");
 	MY_DEBUG("targets = (");
 	my_list_iter(&my_targets, my_target_dump_fn, NULL);
-	MY_DEBUG(");");
-
-}
-
-#endif /* MY_DEBUGGING */
-
-static my_port_impl_t *my_source_impl_find(my_port_conf_t *conf)
-{
-	my_port_impl_t *impl;
-	my_node_t *node;
-	char *impl_name;
-
-	impl_name = my_prop_lookup(conf->properties, "type");
-	if (!impl_name) {
-		impl_name = "file";
-	}
-
-	impl = my_port_impl_lookup(&my_sources, impl_name);
-	if (!impl) {
-		my_log(MY_LOG_ERROR, "core/%s: unknown type '%s' for source #%d '%s'", conf->name, impl_name);
-	}
-
-	return impl;
-}
-
-static int my_source_create_fn(void *data, void *user, int flags)
-{
-	my_core_t *core = MY_CORE(user);
-	my_port_t *port;
-	my_port_conf_t *conf = MY_PORT_CONF(data);
-	my_port_impl_t *impl;
-
-	impl = my_source_impl_find(conf);
-	if (!impl) {
-		return 0;
-	}
-
-	port = my_port_create(core, conf, impl);
-	if (port) {
-		my_list_enqueue(core->sources, port);
-	}
-
-	return 0;
-}
-
-int my_source_create_all(my_core_t *core, my_conf_t *conf)
-{
-	MY_DEBUG("core/sources: creating all");
-	return my_list_iter(conf->sources, my_source_create_fn, core);
-}
-
-int my_source_destroy_all(my_core_t *core)
-{
-	MY_DEBUG("core/sources: destroying all");
-	return my_port_destroy_all(core->sources);
-}
-
-int my_source_open_all(my_core_t *core)
-{
-	MY_DEBUG("core/sources: opening all");
-	return my_port_open_all(core->sources);
-}
-
-int my_source_close_all(my_core_t *core)
-{
-	MY_DEBUG("core/sources: closing all");
-	return my_port_close_all(core->sources);
-}
-
-
-void my_source_register_all(void)
-{
-	MY_SOURCE_REGISTER(file);
-	MY_SOURCE_REGISTER(udp);
-/*
-	MY_SOURCE_REGISTER(sock);
-	MY_SOURCE_REGISTER(http_client);
-	MY_SOURCE_REGISTER(rtp_client);
-*/
-}
-
-#ifdef MY_DEBUGGING
-
-static int my_source_dump_fn(void *data, void *user, int flags)
-{
-	my_port_impl_t *impl = MY_PORT_IMPL(data);
-	MY_DEBUG("\t{");
-	MY_DEBUG("\t\tname=\"%s\";", impl->name);
-	MY_DEBUG("\t\tdescription=\"%s\";", impl->desc);
-	MY_DEBUG("\t}%s", flags & MY_LIST_ITER_FLAG_LAST ? "" : ",");
-
-	return 0;
-}
-
-void my_source_dump_all(void)
-{
-	MY_DEBUG("# registered sources");
-	MY_DEBUG("sources = (");
-	my_list_iter(&my_sources, my_source_dump_fn, NULL);
 	MY_DEBUG(");");
 
 }
